7084a37818acd68922ab3fe4c1fa6568c7f4c771
Mohamed Emad <137570256+TheBestPG22@users.noreply.github.com>
2024-11-14 18:37:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-class Add_num:
-        def adder(self , a , b):
-            return a + b
+import unittest
 
+from main import Add_num
+class AdderTester(unittest.TestCase):
+
+    def setUp(self):
+        # This method is run before each test, useful for setup code.
+        self.calc = Add_num()
+
+    def test_add(self):
+        result =  self.calc.adder(1 , 6)
+        self.assertEqual(result, 7)
+
+        result =  self.calc.adder(11 , 0)
+        self.assertEqual(result, 11)
+
+        result =  self.calc.adder(-1 , 0)
+        self.assertEqual(result, -1)
+
+        result =  self.calc.adder(-1 , -6)
+        self.assertEqual(result, -7)
